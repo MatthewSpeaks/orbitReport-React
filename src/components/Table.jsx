045e7be9b9823bb4253ebc37f8cbb5afd11ef995
Matt Speaks <mattspeaks@hotmail.com>
2023-11-17 19:13:16 -0600

    GA6 Orbit Report 9/9 tests passed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,45 @@
+import './styling.css';
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
+     <tr className="tableHeader">
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      {sat.map((data, id)=>{
+        return (
+          <tr key={id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{`${data.operational === true ? "Operational" : "Retired"}`}</td>
+          </tr>
+        )
+      })}
      </tbody>
    </table>
   );
 };
 
 export default Table;
+
+
+/*
+const Buttons = ({ filterByType, setSat, displaySats }) => {
+  // will be 4 total buttoms when the project is complete.
+  return (
+    <>
+    <div>
+      {displaySats.map((sat, id) => {
+    return (
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
+    ); 
+    */
