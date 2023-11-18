59b2d26eba0c4d76d44095e6f8dd7275f1f6ef0e
Matt Speaks <mattspeaks@hotmail.com>
2023-11-18 01:02:52 -0600

    Added styling

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,12 @@
 import satData from "./satData.jsx";
+import './styling.css';
 
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   // will be 4 total buttoms when the project is complete.
   return (
     <>
-    <div>
+    <div className="flex-container">
       {displaySats.map((sat, id) => {
     return (
       <button onClick={() => filterByType(sat)} key={id}>
